7680ce84e53e94a50511529d07f4a96dfe292407
Ivan Chabanov <firstchabanov@gmail.com>
2026-01-19 11:12:10 +0300

    Fix routing problems

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,12 @@ import './i18n'
 import './index.css'
 import App from './App.tsx'
 
+const redirectParam = new URLSearchParams(window.location.search).get('p')
+if (redirectParam) {
+  const decodedPath = decodeURIComponent(redirectParam)
+  window.history.replaceState(null, '', decodedPath)
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
